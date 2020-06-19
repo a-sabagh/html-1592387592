21ff0891337f1bd08b6bc288c5dec298e78702b8
a-sabagh <a.sabagh72@gmail.com>
2020-06-19 18:45:43 +0430

    lesson three column slick slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,39 @@
- 
+jQuery(document).ready(function($){
+    $('.lesson-three-column-slider').slick({
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        rtl: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 980,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                }
+            }
+        ],
+        prevArrow: '<div class="slick-prev"></div>',
+        nextArrow: '<div class="slick-next"></div>'    
+    });
+}); 

5f6720416ee7c6db8fe5f3332e4683752a0b584b
a-sabagh <a.sabagh72@gmail.com>
2020-06-19 19:27:30 +0430

    lesson-four column slider-make slider dynamic column

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,47 @@
 jQuery(document).ready(function($){
-    $('.lesson-column-slider').slick({
-        dots: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        rtl: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: false
+    var slickSlider = $('.i-slick-slider');
+    slickSlider.each(function(index){
+        let column = $(this).data('column');
+        let largeColumn = parseInt(column);
+        console.log(largeColumn);
+        let mediumColumn = parseInt(largeColumn-1);
+        let smallColumn = 1;
+        $(this).slick({
+            dots: false,
+            slidesToShow: largeColumn,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            rtl: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: largeColumn,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: false
+                    }
+                },
+                {
+                    breakpoint: 980,
+                    settings: {
+                        slidesToShow: mediumColumn,
+                        slidesToScroll: 2,
+                        dots: false,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: smallColumn,
+                        slidesToScroll: 1,
+                        dots: false,
+                    }
                 }
-            },
-            {
-                breakpoint: 980,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    dots: false,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: false,
-                }
-            }
-        ],
-        prevArrow: '<div class="slick-prev"></div>',
-        nextArrow: '<div class="slick-next"></div>'    
+            ],
+            prevArrow: '<div class="slick-prev"></div>',
+            nextArrow: '<div class="slick-next"></div>'    
+        });
     });
 }); 
